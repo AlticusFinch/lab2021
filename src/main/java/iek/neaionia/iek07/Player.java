359bf9ede91fa145ba2d1d360c89885dc0b263b1
pc1 <pc1@192.168.0.195>
2021-11-16 20:01:52 +0200

    δημιουργία νέου project

---
+++ src/main/java/iek/neaionia/iek07/Player.java
@@ -0,0 +1,14 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package iek.neaionia.iek07;
+
+/**
+ *
+ * @author pc1
+ */
+public class Player {
+    
+}
